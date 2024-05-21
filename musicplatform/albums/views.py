7a03633c9dab2ae74bf48cec45c28e77bec8e6e0
Omar Khaled <okhaled9@gmail.com>
2024-05-21 15:28:37 +0300

    implemented custom permissions for songs by overriding views

--- musicplatform/albums/views.py
+++ musicplatform/albums/views.py
@@ -92,3 +92,70 @@ class SongViewSet(viewsets.ModelViewSet):
     queryset = Song.objects.all()
     serializer_class = SongSerializer
     filterset_class = SongFilter
+
+    def create(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return Response({"detail":"No authentication provided"},status=status.HTTP_401_UNAUTHORIZED)
+        
+        try:
+            Artist.objects.get(user=request.user)
+        except Exception:
+            return Response({"detail":"This user is not an artist"},status=status.HTTP_403_FORBIDDEN)
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        album_id = request.data.get("album")        
+        album_obj = Album.objects.get(id=album_id)
+        user = album_obj.artist.user
+        
+        if request.user != user:
+            return Response({"detail":"You do not have permission to access another user's resources"},status=status.HTTP_403_FORBIDDEN)
+
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def destroy(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return Response({"detail":"No authentication provided"},status=status.HTTP_401_UNAUTHORIZED)
+
+        instance = self.get_object()
+
+        user = instance.album.artist.user
+        if request.user != user:
+            return Response({"detail":"You do not have permission to access another user's resources"},status=status.HTTP_403_FORBIDDEN)
+
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    def update(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return Response({"detail":"No authentication provided"},status=status.HTTP_401_UNAUTHORIZED)
+
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+
+        user = instance.album.artist.user
+        if request.user != user:
+            return Response({"detail":"You do not have permission to access another user's resources"},status=status.HTTP_403_FORBIDDEN)
+        
+        target_album_id = request.data.get("album")
+        try:
+            target_album_obj = Album.objects.get(id=target_album_id)
+        except Exception:
+            return Response({"detail":"album not found"}, status=status.HTTP_404_NOT_FOUND)
+        
+        target_album_user = target_album_obj.artist.user
+        if request.user != target_album_user:
+            return Response({"detail":"cannot allocate song to another user's album"},status=status.HTTP_403_FORBIDDEN)
+
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+        return Response(serializer.data)
